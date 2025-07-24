da03ecdb009447d5dae5dacb2b4d35daaa180f70
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-07-24 14:42:53 +0000

    home page setup

---
+++ src/app/pages/main/main.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { MainRoutingModule } from './main-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }
